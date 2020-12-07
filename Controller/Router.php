53629419e14922f6d527d075244b253d9e51ea99
Gianni Giudice <45078018+GianniGiudice@users.noreply.github.com>
2020-12-08 00:52:01 +0100

    Create Router.php

---
+++ Controller/Router.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'Controller/HomeController.php';
+require_once 'View/View.php';
+class Router {
+    private $homeController;
+
+    public function __construct() {
+        $this->homeController = new HomeController();
+    }
+
+    // Route une requête entrante : exécution l'action associée
+    public function routerRequest() {
+        try {
+            if (isset($_GET['action'])) {
+                // Aucune action définie : affichage de l'accueil
+                $this->homeController->accueil();
+            }
+        }
+        catch (Exception $e) {
+            $this->error($e->getMessage());
+        }
+    }
+
+    // Affiche une erreur
+    private function error($errorMessage) {
+        $view = new View("Erreur");
+        $view->generate(array('errorMessage' => $errorMessage));
+    }
+
+    // Recherche un paramètre dans un tableau
+    private function getParameter($array, $name) {
+        if (isset($array[$name])) {
+            return $array[$name];
+        }
+        else
+            throw new Exception("Paramètre '$name' absent");
+    }
+
+}
