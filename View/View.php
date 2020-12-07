c8175003e0934fa2a830cb87ca486bf73311236a
Gianni Giudice <45078018+GianniGiudice@users.noreply.github.com>
2020-12-08 00:57:01 +0100

    Create View.php

---
+++ View/View.php
@@ -0,0 +1,46 @@
+<?php
+
+class View {
+
+    // Nom du fichier associé à la vue
+    private $filename;
+    
+    // Titre de la vue (défini dans le fichier vue)
+    private $title;
+
+    public function __construct($action) {
+        // Détermine le nom du fichier vue à partir de l'action
+        $this->filename = "View/view" . $action . ".php";
+    }
+
+    // Génère et affiche la vue
+    public function generate($data) {
+        // Génération de la partie spécifique de la vue
+        $content = $this->generateFile($this->filename, $data);
+        // Génération du template commun utilisant la partie spécifique
+        $view = $this->generateFile('View/template.php', [
+                'title' => $this->title, 'content' => $content
+        ]);
+        // Renvoi de la vue au navigateur
+        echo $view;
+    }
+
+    // Génère un fichier vue et renvoie le résultat produit
+    private function generateFile($filename, $data) {
+        if (file_exists($filename)) {
+            // Rend les éléments du tableau $donnees accessibles dans la vue
+            extract($data);
+            // Démarrage de la temporisation de sortie
+            ob_start();
+            // Inclut le fichier vue
+            // Son résultat est placé dans le tampon de sortie
+            require $filename;
+            // Arrêt de la temporisation et renvoi du tampon de sortie
+            return ob_get_clean();
+        }
+        else {
+            throw new Exception("Fichier '$filename' introuvable");
+        }
+    }
+
+}
